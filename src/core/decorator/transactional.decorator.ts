ff0fe2b080072459edd69a184ecfdb40675e6817
wonjjee <yoteamo7@naver.com>
2024-01-31 18:43:01 +0900

    feat: added Transactional decorator

---
+++ src/core/decorator/transactional.decorator.ts
@@ -0,0 +1,42 @@
+import { InternalServerErrorException } from '@nestjs/common';
+import { getNamespace } from 'cls-hooked';
+import {
+  THINGS_ENTITY_MANAGER,
+  THINGS_NAMESPACE,
+} from 'src/common/constant/nameSpace';
+import { EntityManager } from 'typeorm';
+
+export function Transactional() {
+  return function (
+    _target: Object,
+    _propertyKey: string | symbol,
+    descriptor: TypedPropertyDescriptor<any>,
+  ) {
+    // save original method
+    const originMethod = descriptor.value;
+
+    // wrapped origin method with Transaction
+    async function transactionWrapped(...args: unknown[]) {
+      // validate nameSpace && get nameSpace
+      const nameSpace = getNamespace(THINGS_NAMESPACE);
+      if (!nameSpace || !nameSpace.active)
+        throw new InternalServerErrorException(
+          `${THINGS_NAMESPACE} is not active`,
+        );
+
+      // get EntityManager
+      const em = nameSpace.get(THINGS_ENTITY_MANAGER) as EntityManager;
+      if (!em)
+        throw new InternalServerErrorException(
+          `Could not find EntityManager in ${THINGS_NAMESPACE} nameSpace`,
+        );
+
+      return await em.transaction(async (tx: EntityManager) => {
+        nameSpace.set(THINGS_ENTITY_MANAGER, tx);
+        return await originMethod.apply(this, args);
+      });
+    }
+
+    descriptor.value = transactionWrapped;
+  };
+}

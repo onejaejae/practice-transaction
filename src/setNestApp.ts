33a41186725a6bf9d4c01958eb53ab5ceb48a0ec
wonjjee <yoteamo7@naver.com>
2024-01-31 18:46:58 +0900

    feat: updated setNestApp for 직렬화, 역직렬화

--- src/setNestApp.ts
+++ src/setNestApp.ts
@@ -1,3 +1,16 @@
-import { INestApplication } from '@nestjs/common';
+import {
+  ClassSerializerInterceptor,
+  INestApplication,
+  ValidationPipe,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 
-export function setNestApp<T extends INestApplication>(app: T): void {}
+export function setNestApp<T extends INestApplication>(app: T): void {
+  app.setGlobalPrefix('api');
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+}

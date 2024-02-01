fed2fc06cbedb076e7534ca900eed875a685aaa8
wonjjee <yoteamo7@naver.com>
2024-02-01 11:09:01 +0900

    feat: apply Transactional decorator in itemService, fix typo

--- src/modules/item/item.service.ts
+++ src/modules/item/item.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { CreateBonusItemDto } from 'src/common/request/item/create-bonus-item.dto';
 import { CreateCommonItemQueryDto } from 'src/common/request/item/create-common-item.query.dto';
+import { Transactional } from 'src/core/decorator/transactional.decorator';
 import { ItemRepository } from 'src/entities/item/item.repository';
 import { UserRepository } from 'src/entities/user/user.repository';
 
@@ -8,9 +9,10 @@ import { UserRepository } from 'src/entities/user/user.repository';
 export class ItemService {
   constructor(
     private readonly userRepository: UserRepository,
-    private readonly itemRepostory: ItemRepository,
+    private readonly itemRepository: ItemRepository,
   ) {}
 
+  @Transactional()
   async createCommonItem(
     userId: number,
     createCommonItemQueryDto: CreateCommonItemQueryDto,
@@ -18,13 +20,14 @@ export class ItemService {
     await this.userRepository.findByIdOrThrow(userId);
     const commonItemEntiy = createCommonItemQueryDto.toEntity(userId);
 
-    return this.itemRepostory.createEntity(commonItemEntiy);
+    return this.itemRepository.createEntity(commonItemEntiy);
   }
 
+  @Transactional()
   async createBonusItem(createBonusItemDto: CreateBonusItemDto) {
     await this.userRepository.findByIdOrThrow(createBonusItemDto.userId);
     const bonusItemEntiy = createBonusItemDto.toEntity();
 
-    return this.itemRepostory.createEntity(bonusItemEntiy);
+    return this.itemRepository.createEntity(bonusItemEntiy);
   }
 }

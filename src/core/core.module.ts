6a8ff9a0c3f922813ca2032719e09b696dac1100
wonjjee <yoteamo7@naver.com>
2024-02-01 23:59:21 +0900

    feat: added ApiResponseInterceptor

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -13,11 +13,13 @@ import { RedisModule } from './database/redis/redis.module';
 import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { TypeORMExceptionFilter } from './filter/typeorm.exception.filter';
 import { ErrorInterceptor } from './interceptor/error.interceptor';
+import { ApiResponseInterceptor } from './interceptor/apiResponse.interceptor';
 
 const modules = [ThingsConfigModule, RedisModule];
 const providers = [TransactionManager];
 const interceptors: ClassProvider[] = [
   { provide: APP_INTERCEPTOR, useClass: ErrorInterceptor },
+  { provide: APP_INTERCEPTOR, useClass: ApiResponseInterceptor },
 ];
 const filters: ClassProvider[] = [
   { provide: APP_FILTER, useClass: TypeORMExceptionFilter },

64c4b0c312b8051d338f482e7f931e9449353a33
wonjjee <yoteamo7@naver.com>
2024-02-02 00:36:29 +0900

    feat: added start log

--- src/main.ts
+++ src/main.ts
@@ -5,17 +5,22 @@ import { setNestApp } from './setNestApp';
 import { ThingsConfigService } from './core/config/config.service';
 import cookieParser from 'cookie-parser';
 import session from 'express-session';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(Modules);
 
   const configService = app.get(ThingsConfigService);
+  const appConfig = configService.getAppConfig();
   const serverConfig = configService.getServer();
 
   app.use(cookieParser());
   app.use(session(serverConfig.SESSION));
 
   setNestApp(app);
-  await app.listen(3000);
+
+  await app.listen(appConfig.PORT);
+  Logger.log(`🧐 [THINGS-API][${appConfig.ENV}] Started at: ${Date.now()}`);
+  Logger.log(`🚀 Server open at ${appConfig.BASE_URL}:${appConfig.PORT}`);
 }
 bootstrap();

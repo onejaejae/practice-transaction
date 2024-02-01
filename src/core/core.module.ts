c80ddcd6f473832fba18384355165643c9858322
wonjjee <yoteamo7@naver.com>
2024-02-01 23:57:00 +0900

    feat: added TypeORMExceptionFilter

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,17 +1,28 @@
-import { Global, MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import {
+  ClassProvider,
+  Global,
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+} from '@nestjs/common';
 import { ThingsConfigModule } from './config/config.module';
 import { getTypeOrmModule } from './database/typeorm/typeorm.module';
 import { TransactionManager } from './database/typeorm/transaction.manager';
 import { TransactionMiddleware } from './middleware/transaction.middleware';
 import { RedisModule } from './database/redis/redis.module';
+import { APP_FILTER } from '@nestjs/core';
+import { TypeORMExceptionFilter } from './filter/typeorm.exception.filter';
 
 const modules = [ThingsConfigModule, RedisModule];
 const providers = [TransactionManager];
+const filters: ClassProvider[] = [
+  { provide: APP_FILTER, useClass: TypeORMExceptionFilter },
+];
 
 @Global()
 @Module({
   imports: [getTypeOrmModule(), ...modules],
-  providers: [...providers],
+  providers: [...providers, ...filters],
   exports: [...modules, ...providers],
 })
 export class CoreModule implements NestModule {
